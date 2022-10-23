36ec2f249beb6939a3f65477c20595bd379b5890
zkxkckvk789 <zkxkckvk789@naver.com>
2022-10-23 19:27:42 +0900

    하이

---
+++ webstudy66-mjh1023/src/main/java/org/kosta/model/MemberVO.java
@@ -0,0 +1,21 @@
+package org.kosta.model;
+
+public class MemberVO {
+	private String id;
+
+	public MemberVO() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	
+	
+}
